d9625257cc64830425b8b9be682ee43d1b6adc99
Rodrigo Morais <rmoraisu@protonmail.com>
2023-04-18 10:59:19 -0500

    got started on move out of range

--- ai.ts
+++ ai.ts
@@ -23,12 +23,20 @@ export class AI {
   }
 
   makeTurn(game: Tridice) {
-    if (!this.tryCapture(game)) {
-    } else console.log("capture successful");
-    game.endTurn();
+    console.log("Making turn");
+    if (this.tryCapture(game)) {
+      console.log("Capture successful");
+      return game.endTurn();
+    }
+    console.log("Capture unsuccessful, tyring move out of range");
+    if (this.tryMoveOutOfRange(game)) {
+      console.log("Move out of range successful");
+      return game.endTurn();
+    }
   }
 
   tryCapture(game: Tridice) {
+    console.log("Trying capture");
     const allPlacedDice = game.board();
     const player = game.currentPlayer;
 
@@ -67,22 +75,30 @@ export class AI {
 
     if (possibleCapture === undefined) return false;
 
+    console.log(`Capture successful: ${possibleCapture.path}`);
+
     game.selectDice(possibleCapture.originDice);
     game.captureDiceAt(possibleCapture.capture);
 
     const { path } = possibleCapture;
 
-    for (const [idx, cell1] of path.entries()) {
-      if (idx + 1 >= path.length) continue;
-      const cell2 = path[idx + 1];
-      const direction = AI.direction(cell1, cell2);
-      game.moveDice(direction);
-    }
+    AI.moveDiceThroughPath(game, path);
 
     return true;
   }
 
+  static moveDiceThroughPath(game: Tridice, path: CellID[]) {
+    for (const [idx, cell1] of path.entries()) {
+      if (idx + 1 >= path.length) continue;
+      const cell2 = path[idx + 1];
+      const direction = AI.direction(cell1, cell2);
+      console.log(`Moving ${direction}`);
+      game.moveDice(direction);
+    }
+  }
+
   tryMoveOutOfRange(game: Tridice) {
+    console.log("Trying to move out of range");
     const diceOnBoard = game.board();
     const playerDiceOnBoard = game.currentPlayer.diceOnBoard;
     for (const cellWithDice of diceOnBoard.keys()) {
@@ -105,19 +121,31 @@ export class AI {
   moveOutOfRange(dice: Dice, attacker: Dice, game: Tridice): boolean {
     game.selectDice(dice);
 
-    let canEscape = false;
+    console.log(`Attacker: ${attacker.cell!.id}`);
+
+    let escapeRoute: CellID[] = [];
 
     const escapeRoutes = this.reachFrom(dice.cell!.id, dice.topFace);
+    console.log(`Possible escape routes: `, escapeRoutes);
     escapeRoutes.forEach((path, endCell) => {
+      console.log(`Trying ${path} to ${endCell}`);
       if (path.length !== dice.topFace) return;
-      const validEscapes: CellID[][] = [];
       attacker.owner.diceOnBoard.forEach((enemyDice) => {
-        if (this.diceCanReachCell(enemyDice, endCell)) return;
-        validEscapes.push(path);
+        if (this.diceCanReachCell(enemyDice, endCell) || escapeRoute.length > 0)
+          return;
+        escapeRoute = [...path, endCell];
       });
     });
 
-    return canEscape;
+    if (escapeRoute.length > 0) {
+      console.log(`Escaping through ${escapeRoute}`);
+      AI.moveDiceThroughPath(game, escapeRoute);
+      return true;
+    }
+
+    game.deselectDice();
+
+    return false;
   }
 
   diceCanReachCell(
@@ -230,12 +258,18 @@ function tester() {
   const game = new Tridice();
   const ai = new AI();
 
+  placeDice(game, 11, game.currentPlayer.getDice(0));
   placeDice(game, 33, game.currentPlayer.getDice(0));
-  placeDice(game, 32, game.currentPlayer.getDice(0));
+  const enemyDice = game.nextPlayer.getDice(0)!;
+  const aiDice = game.currentPlayer.getDice(0)!;
+  while (enemyDice.topFace !== 4 || !game.getCellAt(33)?.diceFits(enemyDice))
+    enemyDice.simulatedRoll(3);
+  while (aiDice.topFace !== 1 || !game.getCellAt(11)?.diceFits(aiDice))
+    aiDice.simulatedRoll(3);
 
-  // ai.makeTurn(game);
+  ai.makeTurn(game);
 
-  // console.log(game.board());
+  console.log(game.board());
   // console.log(game.currentPlayer.lostDice, game.currentPlayer.id);
 }
 
